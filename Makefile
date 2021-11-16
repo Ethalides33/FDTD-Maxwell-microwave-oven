ad10b1976ae8267445370db97605cd75fba62be1
Finta Ionut <ifinta@student.uliege.be>
2021-11-17 00:10:57 +0100

    Silo implementation Makefile correction Gitignore executable

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 SRC=main.c
 EXE=microwave
 all:
+	@echo "If you get error 'not found silo library', run 'make h5'"
 	gcc ${SRC} -lm -lsilo -std=c99 -o ${EXE}
+
+h5:
+	gcc ${SRC} -lm -lsiloh5 -std=c99 -o ${EXE}
 
 parallel:
 	# Check to add MPI library ?
--- main.c
+++ main.c
@@ -12,14 +12,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <silo.h>
 #include <string.h>
 
-#ifndef EXIT_FAILURE
-#define EXIT_FAILURE 1
-#endif
+#define DB_FILENAME "result.silo"
+#define DB_MESHNAME "mesh"
 
 #define MU 1.25663706143591729538505735331180115367886775975E-6
 #define EPSILON 8.854E-12
+#define PI 3.14159265358979323846264338327950288419716939937510582097494
 
 /**
 ------------------------------------------------------
@@ -36,6 +37,9 @@ typedef unsigned char uchar;
  *    width:            a in figure (y, in paper cs)
  *    height:           b in figure (z, in paper cs)
  *    length:           d in figure (x, in paper cs)
+ *    maxi:             Number of grid subdivisions (x dimension)
+ *    maxj:             Number of grid subdivisions (y dimension)
+ *    maxk:             Number of grid subdivisions (z dimension)
  *    spatial_step:     delta x = delta y = delta z
  *    time_step:        delta t
  *    simulation_time:  interval of time simulated (e.g. 1s)
@@ -56,6 +60,11 @@ typedef struct parameters
 
 } Parameters;
 
+/** A structure that rassembles all the fields components
+ * Properties:
+ *      E_x/y/z     The arrays of the x/y/z components of the electric field
+ *      H_x/y/z     The arrays of the x/y/z componnents of the magnetic field
+ **/
 typedef struct fields
 {
 
@@ -68,14 +77,20 @@ typedef struct fields
 
 } Fields;
 
-/** Exceptionnally clever way to garbage collect **/
+/** Exceptionnally clever way to garbage collect
+ * Parameters:
+ *      previous:   The previous allocated object
+ *      ptr:        The current allocated object
+ * Description:
+ *      This structure allow us to store each allocated object
+ *      into a chained list with a LiFo strategy so when we
+ *      free up memory we take care of the inner most objects
+**/
 typedef struct chainedAllocated
 {
     struct chainedAllocated *previous;
     void *ptr;
 } ChainedAllocated;
-
-static ChainedAllocated *allocatedLs;
 
 /**
 ------------------------------------------------------
@@ -84,8 +99,17 @@ static ChainedAllocated *allocatedLs;
 ------------------------------------------------------
 **/
 
-/** Free any allocated array **/
-void *free_malloced()
+// Superglobal variable
+static ChainedAllocated *allocatedLs;
+
+/** Free any allocated object with the next Malloc function
+ * Description:
+ *  This function free's the memory allocated for all
+ *  the objects allocated with the next Malloc function
+ *  in the reverse way of their allocation (LiFo) in order
+ *  to deal with nested structures.
+**/
+void *freeAll()
 {
     while (allocatedLs)
     {
@@ -96,14 +120,28 @@ void *free_malloced()
     }
 }
 
-/** Critical allocation shortcut (malloc or Critical error with exit) **/
-void *malloc_and_check(size_t __size)
+/** Free the memory and throws an error, then exits.**/
+void fail(const char *msg)
+{
+    perror(msg);
+    freeAll();
+    exit(EXIT_FAILURE);
+}
+
+/** Critical allocation shortcut (malloc or Critical error with exit)
+ * Description:
+ *  In addition to critically check if the malloc properly worked,
+ *  this function stores the reference to the new allocated object
+ *  in the superglobal chained list which is then used to free the
+ *  memory before exit.
+**/
+void *Malloc(size_t size)
 {
     if (allocatedLs == NULL)
     {
         allocatedLs = malloc(sizeof(ChainedAllocated));
     }
-    void *ptr = malloc(__size);
+    void *ptr = malloc(size);
 
     allocatedLs->ptr = ptr;
     ChainedAllocated *successor = malloc(sizeof(ChainedAllocated));
@@ -112,9 +150,7 @@ void *malloc_and_check(size_t __size)
 
     if (!ptr)
     {
-        perror("CRITICAL ERROR: Could not allocate enough memory!");
-        free_malloced();
-        exit(EXIT_FAILURE);
+        fail("CRITICAL ERROR: Could not allocate enough memory!");
     }
     return ptr;
 }
@@ -125,26 +161,25 @@ void *malloc_and_check(size_t __size)
  * Return value:
  *    A pointer to the parameters structure loaded in system memory
 **/
-Parameters *load_parameters(char *filename)
+Parameters *load_parameters(const char *filename)
 {
-    FILE *pParameters_file = fopen(filename, "r");
-    Parameters *pParameters = malloc_and_check(sizeof(Parameters));
+    FILE *fParams = fopen(filename, "r");
+    Parameters *pParameters = Malloc(sizeof(Parameters));
 
-    if (!pParameters_file)
+    if (!fParams)
     {
-        perror("Unable to open parameters file!");
-        exit(EXIT_FAILURE);
+        fail("Unable to open parameters file!");
     }
 
-    fscanf(pParameters_file, "%f", &pParameters->width);
-    fscanf(pParameters_file, "%f", &pParameters->height);
-    fscanf(pParameters_file, "%f", &pParameters->length);
-    fscanf(pParameters_file, "%lf", &pParameters->spatial_step);
-    fscanf(pParameters_file, "%lf", &pParameters->time_step);
-    fscanf(pParameters_file, "%f", &pParameters->simulation_time);
-    fscanf(pParameters_file, "%u", &pParameters->sampling_rate);
+    fscanf(fParams, "%f", &pParameters->width);
+    fscanf(fParams, "%f", &pParameters->height);
+    fscanf(fParams, "%f", &pParameters->length);
+    fscanf(fParams, "%lf", &pParameters->spatial_step);
+    fscanf(fParams, "%lf", &pParameters->time_step);
+    fscanf(fParams, "%f", &pParameters->simulation_time);
+    fscanf(fParams, "%u", &pParameters->sampling_rate);
 
-    fclose(pParameters_file);
+    fclose(fParams);
 
     pParameters->maxi = (uint)(pParameters->length / pParameters->spatial_step + 1);
     pParameters->maxj = (uint)(pParameters->width / pParameters->spatial_step + 1);
@@ -153,18 +188,22 @@ Parameters *load_parameters(char *filename)
     return pParameters;
 }
 
-//It might be possible to only store data for one time step.
+/** Allocates and initialize to 0.0 all the components of each field at a given time t.
+ * Comment:
+ *  (Amaury) It might be possible to store in system memory only the data for a single time step.
+ *  (Ionut) Pay attention that I/O operations can be bottleneck... ?
+**/
 Fields *initialize_fields(Parameters *params)
 {
-    Fields *pFields = malloc_and_check(sizeof(Fields));
+    Fields *pFields = Malloc(sizeof(Fields));
     uint space_size = params->maxi * params->maxj * params->maxk;
 
-    pFields->E_x = malloc_and_check(sizeof(double) * space_size);
-    pFields->E_y = malloc_and_check(sizeof(double) * space_size);
-    pFields->E_z = malloc_and_check(sizeof(double) * space_size);
-    pFields->H_x = malloc_and_check(sizeof(double) * space_size);
-    pFields->H_y = malloc_and_check(sizeof(double) * space_size);
-    pFields->H_z = malloc_and_check(sizeof(double) * space_size);
+    pFields->E_x = Malloc(sizeof(double) * space_size);
+    pFields->E_y = Malloc(sizeof(double) * space_size);
+    pFields->E_z = Malloc(sizeof(double) * space_size);
+    pFields->H_x = Malloc(sizeof(double) * space_size);
+    pFields->H_y = Malloc(sizeof(double) * space_size);
+    pFields->H_z = Malloc(sizeof(double) * space_size);
 
     for (uint i = 0; i < space_size; ++i)
     {
@@ -179,16 +218,16 @@ Fields *initialize_fields(Parameters *params)
     return pFields;
 }
 
-/** Sets the initial fields **/
+/** Sets the initial field as asked in Question 3.a. **/
 void set_initial_conditions(double *E_y, Parameters *params)
 {
     for (uint i = 0; i < params->maxi; ++i)
     {
-        for (uint j = 0; j < params->maxj; ++j)
+        for (uint j = 0; j < params->maxj ^ 2; j += params->maxj)
         {
             for (uint k = 0; k < params->maxk; ++i)
             {
-                E_y[i + j * params->maxj + k * params->maxk * params->maxj] = sin(1 * M_PI * i * params->spatial_step / params->width) * sin(1 * M_PI * k * params->spatial_step / params->length);
+                E_y[i + j + k * params->maxk * params->maxj] = sin(1 * PI * i * params->spatial_step / params->width) * sin(1 * PI * k * params->spatial_step / params->length);
             }
         }
     }
@@ -312,8 +351,17 @@ void update_E_z_field(Parameters *params, double *E_z, double *H_y, double *H_x)
             }
 }
 
-void propagate_fields(Fields *pFields, Parameters *pParams)
+void propagate_fields(Fields *pFields, Parameters *pParams, DBfile *db)
 {
+    int dims[] = {pParams->length, pParams->width, pParams->height};
+    int ndims = 3;
+
+    DBPutQuadvar1(db, "E_x", DB_MESHNAME, pFields->E_x, dims, ndims, NULL, 0, DB_DOUBLE, DB_NODECENT, NULL);
+    DBPutQuadvar1(db, "E_y", DB_MESHNAME, pFields->E_y, dims, ndims, NULL, 0, DB_DOUBLE, DB_NODECENT, NULL);
+    DBPutQuadvar1(db, "E_z", DB_MESHNAME, pFields->E_z, dims, ndims, NULL, 0, DB_DOUBLE, DB_NODECENT, NULL);
+    DBPutQuadvar1(db, "H_x", DB_MESHNAME, pFields->H_x, dims, ndims, NULL, 0, DB_DOUBLE, DB_NODECENT, NULL);
+    DBPutQuadvar1(db, "H_y", DB_MESHNAME, pFields->H_y, dims, ndims, NULL, 0, DB_DOUBLE, DB_NODECENT, NULL);
+    DBPutQuadvar1(db, "H_z", DB_MESHNAME, pFields->H_z, dims, ndims, NULL, 0, DB_DOUBLE, DB_NODECENT, NULL);
 
     float time_counter;
     for (time_counter = 0; time_counter <= pParams->simulation_time; time_counter += pParams->time_step)
@@ -329,15 +377,45 @@ void propagate_fields(Fields *pFields, Parameters *pParams)
     }
 }
 
+/** Draw the oven mesh with all the girds **/
+void draw_oven(Parameters *params, DBfile *db)
+{
+    //TODO: Maybe create a Free to be able to free these once written?
+    float *x = Malloc(params->maxi * sizeof(float));
+    float *y = Malloc(params->maxj * sizeof(float));
+    float *z = Malloc(params->maxk * sizeof(float));
+
+    //TODO: Optimization: iterate once to the bigger and affect if in bounds of array...
+    for (int i = 0; i < params->maxi; ++i)
+    {
+        x[i] = i;
+    }
+
+    for (int i = 0; i < params->maxj; ++i)
+    {
+        y[i] = i;
+    }
+
+    for (int i = 0; i < params->maxk; ++i)
+    {
+        z[i] = i;
+    }
+
+    int dims[] = {params->length, params->width, params->height};
+    int ndims = 3;
+    float *cords[] = {x, y, z};
+    DBPutQuadmesh(db, "oven", NULL, cords, dims, ndims, DB_DOUBLE, DB_COLLINEAR, NULL);
+}
+
 /**
 ------------------------------------------------------
 ---------------------------------------- Main function
 ------------------------------------------------------
 **/
 
-int main(int argc, char **argv)
+int main(int argc, const char *argv[])
 {
-    printf("Welcome into our microwave oven simulation engine! \n");
+    printf("Welcome into our microwave oven eletrico-magnetic field simulator! \n");
 
     if (argc != 2)
     {
@@ -347,6 +425,8 @@ int main(int argc, char **argv)
 
     printf("Loading the parameters...\n");
     Parameters *pParameters = load_parameters(argv[1]);
+
+    //BEGIN DEBUG
     printf("width: %f \n", pParameters->width);
     printf("height: %f \n", pParameters->height);
     printf("length: %f \n", pParameters->length);
@@ -354,6 +434,8 @@ int main(int argc, char **argv)
     printf("time: %lf \n", pParameters->time_step);
     printf("total: %f \n", pParameters->simulation_time);
     printf("rate: %u \n", pParameters->sampling_rate);
+    //END DEBUG
+
     if (pParameters->time_step > pParameters->simulation_time)
     {
         perror("The time step must be lower than the simulation time!");
@@ -361,11 +443,21 @@ int main(int argc, char **argv)
     }
 
     Fields *pFields = initialize_fields(pParameters);
-    set_initial_conditions(pFields, pParameters);
-    propagate_fields(pFields, pParameters);
 
+    // Open a SILO db
+    DBfile *db = DBCreate(DB_FILENAME, DB_CLOBBER, DB_LOCAL, "My first SILO test", DB_PDB);
+    if (!db)
+    {
+        fail("Could not create DB\n");
+    }
+
+    draw_oven(pParameters, db);
+    set_initial_conditions(pFields->E_y, pParameters);
+    propagate_fields(pFields, pParameters, db);
+
+    DBClose(db);
     printf("Freeing memory...\n");
-    free_malloced();
+    freeAll();
 
     printf("Simulation complete!\n");
     return 0;
